3c2f4cdc549bc1289b4537621e7afb1621cdc799
fouad-d <>
2021-08-23 01:45:50 +0100

    update

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const Block blocks[] = {
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	{"",	"music",	0,	11},
 	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
+	/* {"",	"news",		0,	6}, */
 	/* {"",	"crypto",	0,	13}, */
 	/* {"",	"price lbc \"LBRY Token\" 📚",			9000,	22}, */
 	/* {"",	"price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
@@ -14,10 +14,10 @@ static const Block blocks[] = {
 	/* {"",	"price eth Ethereum 🍸",	9000,	23}, */
 	/* {"",	"price btc Bitcoin 💰",				9000,	21}, */
 	/* {"",	"torrent",	20,	7}, */
-	/* {"",	"memory",	10,	14}, */
-	/* {"",	"cpu",		10,	18}, */
+	{"",	"memory",	10,	14}, 
+	/* {"",	"cpu",		10,	18}, */ 
 	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"mailbox",	180,	12},
+	/* {"",	"mailbox",	180,	12},*/
 	{"",	"nettraf",	1,	16},
 	{"",	"forecast",	18000,	5},
 	{"",	"volume",	0,	10},
